2583f855ce94b85d4474fa4a7b215ac604ef1e93
lvl0nax <lvl0nax@gmail.com>
2020-11-08 07:54:41 +0000

    add services, handlers, repository nad migrations

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,31 @@ import (
 	"log"
 
 	"github.com/lvl0nax/tutorial_rest_todo/pkg/handler"
+	"github.com/lvl0nax/tutorial_rest_todo/pkg/repository"
+	"github.com/lvl0nax/tutorial_rest_todo/pkg/service"
+	"github.com/spf13/viper"
 
 	todo "github.com/lvl0nax/tutorial_rest_todo"
 )
 
 func main() {
-	handler := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("Config initializing error: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handler := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 
-	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
 		log.Fatalf("http server run failed. Error: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
